b111e1bcedca8332798f31c20315854c0757fb68
aaronjudd <aaronjudd@me.com>
2014-09-04 09:07:04 -0700

    Packaged for Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  summary: "Meteor package that allows you to securely log in to a non-primary DDP connection from a browser or another server",
+  summary: "Securely log in to a non-primary DDP connection from another browser or server",
   name: "ongoworks:ddp-login",
   version: "0.1.0",
   git: "https://github.com/ongoworks/meteor-ddp-login.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0")
   api.use(['livedata', 'underscore', 'srp', 'sha']);
   api.add_files(['ddp-login.js']);
 });
 
 Package.on_test(function(api) {
-  api.use(['accounts-password', 'ddp-login', 'test-helpers', 'tinytest']);
+  api.use(['accounts-password', 'ongoworks:ddp-login', 'test-helpers', 'tinytest']);
   api.add_files(["ddp-login-tests.js"]);
 });

3ef6d9f5ec065f3dc92de00897fdbe118118052e
jankapunkt <jkuester@uni-bremen.de>
2017-02-16 16:20:07 +0100

    if password is already in hashed format (as in Accounts.onLogin) it is not hashed again

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Securely log in to a non-primary DDP connection from another browser or server",
   name: "ongoworks:ddp-login",
-  version: "0.2.1",
+  version: "0.2.2",
   git: "https://github.com/ongoworks/meteor-ddp-login.git"
 });
 
